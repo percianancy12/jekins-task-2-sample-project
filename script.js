a5e1467dc66ea730b921e4c06d7853da413b38b9
percianancy12 <percianancy@gmail.com>
2025-10-24 21:41:11 +0800

    Third commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function showMessage() {
-  alert("Build triggered successfully!");
+  const output = document.getElementById("output");
+  const now = new Date();
+  output.innerText = `You clicked the button! Current time: ${now.toLocaleTimeString()}`;
 }
